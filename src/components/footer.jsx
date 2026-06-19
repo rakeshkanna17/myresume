e589f4f8ac9a6b61ea8ce060b76e9339a5994392
rakeshkanna500apps <rakesh.kanna@500apps.com>
2026-06-19 19:08:14 +0530

    Deploy: rebuild dist and sync root index.html

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,10 @@
 
 function Footer() {
-  return (
-    <footer>
-  <p>@ 2026 Kanna Rakesh &nbsp;·&nbsp; UI Developer &nbsp;·&nbsp; Hyderabad</p>
-</footer>
-  );
+    return (
+        <footer>
+            <p>@ 2026 Kanna Rakesh &nbsp;·&nbsp; UI Developer &nbsp;·&nbsp; Hyderabad</p>
+        </footer>
+    );
 }
 
 export default Footer;
